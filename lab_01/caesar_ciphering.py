87864cf871be57c863000d344c2506b84f3c5582
Basil Tkachenko <baziltkachenko@gmail.com>
2023-02-10 21:03:15 +0300

    initial commit

---
+++ lab_01/caesar_ciphering.py
@@ -0,0 +1,45 @@
+import string
+import streamlit as st
+
+
+
+
+def caesar_ciphering(data, key):
+    alphabet_lower = string.ascii_lowercase
+    alphabet_upper = string.ascii_uppercase
+    punctuation = string.punctuation
+
+    ciphered_list = []
+
+    print(alphabet_lower)
+    print(len(punctuation))
+
+    for letter in data:
+        if letter.islower():
+            try:
+                ciphered_list.append(alphabet_lower[alphabet_lower.index(letter)+key])
+            except IndexError:
+                ciphered_list.append(alphabet_lower[key - (26 - alphabet_lower.index(letter))])
+        elif letter.isupper():
+            try:
+                ciphered_list.append(alphabet_upper[alphabet_upper.index(letter)+key])
+            except IndexError:
+                ciphered_list.append(alphabet_upper[key - (26 - alphabet_upper.index(letter))])
+        elif letter in punctuation:
+            try:
+                ciphered_list.append(punctuation[punctuation.index(letter) + key])
+            except IndexError:
+                ciphered_list.append(punctuation[key - (32 - punctuation.index(letter))])
+        elif letter.isspace():
+            ciphered_list.append(letter)
+        elif letter.isnumeric():
+            ciphered_list.append(str(int(letter) + key))
+
+    return ''.join(ciphered_list)
+
+
+
+
+ciphered_text = text_ciphering(data, key)
+print(ciphered_text)
+
---
+++ main.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+from lab_01 import caesar_ciphering
+
+
+st.sidebar.image('logo.png', width=300)
+
+
+def header():
+    author = """
+        made by [Basil Tkachenko](https://github.com/Basilt69)
+        for **Ciphering labs**
+        in [BMSTU](https://bmstu.ru) 
+    """
+
+    st.header("BMSTU, University cathedra: Informatics and software management - 7")
+    st.markdown("**Course title:** Data protection")
+    st.markdown("University lecturer: Kivva K.A.")
+    st.markdown("**Student:** Tkachenko B.M.")
+    st.sidebar.markdown(author)
+
+def main():
+    header()
+    lab = st.sidebar.radio(
+        "Select your lab:", (
+            "1. Caesar and Vigenère cipher",
+        ),
+        index=4
+    )
+
+    if lab[:1] == "1":
+        message = st.text_input("Please, input your text")
+        key = st.number_input('Please, input your key(actual shift)')
+        st.write("This our initial message:", message)
+
+        ciphered_message = caesar_ciphering.caesar_ciphering(message, key)
+        st.write("This is what we've got as the result of ciphering:", ciphered_message)
+
+if __name__ == "__main__":
+    main()
+
