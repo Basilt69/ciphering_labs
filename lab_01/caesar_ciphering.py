6d86625ee0cd5fe76da21e6d7b03891c09804e94
Basil Tkachenko <baziltkachenko@gmail.com>
2023-02-13 08:43:58 +0300

    first attempt of caesar dechiphering v.1

--- lab_01/caesar_ciphering.py
+++ lab_01/caesar_ciphering.py
@@ -39,6 +39,36 @@ def caesar_ciphering(data, key):
     return ''.join(ciphered_list)
 
 
+def caesar_deciphering(data, key):
+    alphabet_lower = string.ascii_lowercase
+    alphabet_upper = string.ascii_uppercase
+    punctuation = string.punctuation
+
+    deciphered_list = []
+    for letter in data:
+        if letter.islower():
+            try:
+                deciphered_list.append(alphabet_lower[alphabet_lower.index(letter)-key])
+            except IndexError:
+                deciphered_list.append(alphabet_lower[26 - abs(key - alphabet_lower.index(letter))])
+        elif letter.isupper():
+            try:
+                deciphered_list.append(alphabet_upper[alphabet_upper.index(letter)-key])
+            except IndexError:
+                deciphered_list.append(alphabet_upper[26 - abs(key-alphabet_upper.index(letter))])
+        elif letter in punctuation:
+            try:
+                deciphered_list.append(punctuation[punctuation.index(letter) - key])
+            except IndexError:
+                deciphered_list.append(punctuation[32 - abs(key - punctuation.index(letter))])
+        elif letter.isspace():
+            deciphered_list.append(letter)
+        elif letter.isnumeric():
+            deciphered_list.append(str(int(letter) - key))
+
+    return ''.join(deciphered_list)
+
+
 def main():
     st.markdown("## Laboratory work №1")
     st.markdown("### **Title**: Caesar and Vigenère ciphering")
@@ -58,6 +88,16 @@ def main():
 
     st.markdown("**This is our ciphered message(using Caesar cipher):**")
     st.write(ciphered_msg)
+
+    st.markdown("**Please, input your text to decipher**")
+    deciphering_msg = st.text_input("(All your text, punctuation and numbers will be shifted)")
+
+    st.markdown('**Please, input your key(actual shift)**')
+    key_desc = st.number_input("(numbers shall be integers from 1 to 26)", min_value=0, max_value=26, step=1, value=1)
+
+    deciphered_msg = caesar_deciphering(deciphering_msg, key_desc)
+    st.markdown("**This is our deciphered message(using Caesar cipher):**")
+    st.write(deciphered_msg)
 
 
 if __name__ == "__name__":
