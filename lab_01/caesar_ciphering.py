1623a7aa1face3ad8b1a3c139e97b595d257948f
Basil Tkachenko <baziltkachenko@gmail.com>
2023-02-13 08:50:13 +0300

    first attempt of caesar dechiphering v.3

--- lab_01/caesar_ciphering.py
+++ lab_01/caesar_ciphering.py
@@ -90,7 +90,7 @@ def main():
     st.write(ciphered_msg)
 
     st.markdown("**Please, input your text to decipher**")
-    deciphering_msg = st.text_input("(All your text, punctuation and numbers will be shifted)")
+    deciphering_msg = st.text_input("(All your text, punctuation and numbers shall be shifted)")
 
     st.markdown('**Please, input your key(actual shift)**')
     key_desc = st.number_input("(numbers shall be integers from 1 to 26)", min_value=0, max_value=26, step=1, value=1)
