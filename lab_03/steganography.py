8fabdb8648c2cce3b8a8db751484226c330a3ff5
Basil Tkachenko <baziltkachenko@gmail.com>
2023-03-09 09:04:26 +0300

    lab 3 - steganography v.19

--- lab_03/steganography.py
+++ lab_03/steganography.py
@@ -71,68 +71,6 @@ def get_image(user_img, user_url):
     return img
 
 
-def to_bin(data):
-    #convert data to binary format
-    if isinstance(data, str):
-        return ''.join([format(ord(i), "08b") for i in data])
-    elif isinstance(data, bytes):
-        return ''.join([format(i, "08b") for i in data])
-    elif isinstance(data, np.ndarray):
-        return [format(i, "08b") for i in data]
-    elif isinstance(data, int) or isinstance(data, np.uint8):
-        return format(data, "08b")
-    else:
-        raise TypeError("Type not supported.")
-
-
-def encrypt(image, secret_data):
-    # read the image
-    #image = cv2.imread(image_name)
-
-    #maximum bytes to encode
-    st.write(image.size)
-    st.markdown(image.size[0], image.size[1])
-    #n_bytes = image.shape[0] * image.shape[1] * 3 // 8
-    n_bytes = image.size[0] * image.size[1] * 3 // 8
-
-    st.markdown("[*] maximum bytes to encode:", n_bytes)
-
-    if len(secret_data) > n_bytes:
-        raise ValueError("[!] Insufficient bytes, need bigger image or less data.")
-    st.markdown("Encoding data ...")
-
-    # add stopping criteria
-    secret_data += "====="
-    data_index = 0
-
-    # convert data to binary
-    binary_secret_data = to_bin(secret_data)
-
-    #size of data to hide
-    data_len = len(binary_secret_data)
-
-    for row in image:
-        for pixel in row:
-            # convert RGB values to binary format
-            r, g, b = to_bin(pixel)
-            # modify the least significant bit only if there is still data to store
-            if data_index < data_len:
-                # least significant red pixel bit
-                pixel[0] = int(r[:-1] + binary_secret_data[data_index], 2)
-                data_index += 1
-            if data_index < data_len:
-                # least significant green pixel bit
-                pixel[1] = int(g[:-1] + binary_secret_data[data_index], 2)
-                data_index += 1
-            if data_index < data_len:
-                # least significant blue pixel bit
-                pixel[2] = int(b[:-1] + binary_secret_data[data_index], 2)
-                data_index += 1
-            # if data is encoded, just break out of the loop
-            if data_index >= data_len:
-                break
-    return image
-
 def encrypt_2(img, message):
     keys = []
     draw_object = ImageDraw.Draw(img)
@@ -160,30 +98,6 @@ def decrypt_2(img, keys):
         a.append(pixels[key][0])
     res = ''.join([chr(elem) for elem in a])
     st.write(res)
-
-
-
-def decrypt(image_name):
-    st.markdown("Decoding ...")
-    #read the image
-    image = cv2.imread(image_name)
-
-    binary_data = ""
-    for row in image:
-        for pixel in row:
-            r, g, b = to_bin(pixel)
-            binary_data += r[-1]
-            binary_data += g[-1]
-            binary_data += b[-1]
-    # split by 8 bits
-    all_bytes = [ binary_data[i: i+8] for i in range(0, len(binary_data), 8)]
-    # convert from bits to characters
-    decoded_data = ""
-    for byte in all_bytes:
-        decoded_data += chr(int(byte, 2))
-        if decoded_data[-5:] == "=====":
-            break
-    return decoded_data[:-5]
 
 
 def main():
