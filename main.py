290b421bff2ed3cdd166e76fe3e405c87a0797a3
Basil Tkachenko <baziltkachenko@gmail.com>
2023-03-30 12:33:50 +0300

    lab 5 commit v.3_1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from lab_01 import caesar_ciphering
 from lab_02 import rsa
 from lab_03 import steganography
 from lab_04 import huffman
-from lab_05 import rc4, rc4_v2
+from lab_05 import rc4, rc4_v2, rc4_v3
 
 
 st.sidebar.image('logo.png', width=300)
@@ -50,7 +50,8 @@ def main():
 
     elif lab[:1] == "5":
         #rc4.main()
-        rc4_v2.main()
+        #rc4_v2.main()
+        rc4_v3.main()
 
 
 
